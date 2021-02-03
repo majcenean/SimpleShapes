509c7251e47023d0514a86ccad91a8d669104a05
Maj <maliajenkins13@gmail.com>
2021-02-02 16:13:15 -0800

    initial upload

---
+++ README.md
@@ -0,0 +1,8 @@
+## Hello World
+February 2, 2021
+
+
+### Overview
+https://vimeo.com/507348804
+
+### Technical Details
---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 400);
+  textSize(40);
+  textAlign(CENTER);
+ }
+
+function draw() {
+  background('#484569');
+  fill('#fff');
+  text("Simple Shapes", width/2, height/2)
+}
